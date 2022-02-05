d41b9859beb90ff00822cf8c71a8de72df6c071c
Henri Iser <henri.iser@icloud.com>
2022-02-05 22:07:32 +0100

    work in progress

--- sentence_similarity/corex.py
+++ sentence_similarity/corex.py
@@ -2,42 +2,32 @@ import pickle
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
 import scipy.sparse as ss
 from corextopic import corextopic as ct
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from sentence_similarity.data import (Benchmark, Pipeline, PipelineConfig,
-                                      STSBenchmark)
-
-data_dir = Path("data")
-assert data_dir.exists(), "data_dir does not exist."
-output_dir = Path("data")
-output_dir.mkdir(exist_ok=True, parents=True)
+from sentence_similarity.data import Pipeline
 
 
-def train_corex_model(output_dir: Path, config: PipelineConfig, benchmark: Benchmark):
-    config.save(output_dir)
-    pipeline = Pipeline(config)
-
+def train_corex_model(pipeline: Pipeline, sentences, output_dir: Path, n_hidden=100, max_iter=300, seed=1337):
+    # create output directory
+    output_dir.mkdir(parents=True, exist_ok=True)
+    # save pipeline config
+    pipeline.config.save(output_dir)
     # preprocess sentences
-    s1_preprocessed = pipeline(benchmark.s1)
-    s2_preprocessed = pipeline(benchmark.s2)
-
+    sentences = pipeline(sentences)
     # fit TF-IDF vectorizer
     vectorizer = TfidfVectorizer(strip_accents="ascii", binary=True)
-    doc_word = vectorizer.fit_transform(pd.concat([s1_preprocessed, s2_preprocessed]))
+    doc_word = vectorizer.fit_transform(sentences)
     doc_word = ss.csr_matrix(doc_word)
-
+    # Get words that label the columns (needed to extract readable topics and make anchoring easier)
+    words = list(np.asarray(vectorizer.get_feature_names_out()))
+    # Train the CorEx topic model with 50 topics
+    topic_model = ct.Corex(n_hidden=n_hidden, words=words, max_iter=max_iter, verbose=False, seed=seed)
+    topic_model.fit(doc_word, words=words)
     # save vectorizer
     with open(output_dir / "vectorizer.bin", "wb") as f:
         pickle.dump(vectorizer, f)
-
-    # Get words that label the columns (needed to extract readable topics and make anchoring easier)
-    words = list(np.asarray(vectorizer.get_feature_names_out()))
-
-    # Train the CorEx topic model with 50 topics
-    topic_model = ct.Corex(n_hidden=50, words=words, max_iter=300, verbose=False, seed=1)
-    topic_model.fit(doc_word, words=words)
-
+    # save topic model
     topic_model.save(output_dir / "corex_model.bin")
+    return topic_model, vectorizer
